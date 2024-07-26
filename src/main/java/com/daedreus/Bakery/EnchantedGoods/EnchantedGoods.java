71d315a20bdceed7c0b52a1427852f2d7332fae4
Daedreus <drakosoldier298@gmail.com>
2024-07-26 11:34:43 -0500

    Should add lava depth strider and now relies on additional entity attribute....unsure if this keeps running serverside hopefully it can or i get to remove this cool enchantment

--- src/main/java/com/daedreus/Bakery/EnchantedGoods/EnchantedGoods.java
+++ src/main/java/com/daedreus/Bakery/EnchantedGoods/EnchantedGoods.java
@@ -17,6 +17,12 @@ public class EnchantedGoods implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Enchanting Game!");
+
+
+
+
+
+		LOGGER.info("Enchanted Game!");
 	}
 }
